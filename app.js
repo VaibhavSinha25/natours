9511b34dda508ceb7819cda972a626fb8dbc3fa1
Vaibhav <vaibhavsinha0825@gmail.com>
2024-11-20 20:08:30 +0530

    basic commands of express js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, world!', app: 'Natours' });
+});
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint');
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log('listening on port');
+});
