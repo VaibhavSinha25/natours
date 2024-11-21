f41bced1dbe86ff766864edc0347fb1286c7eb55
Vaibhav <vaibhavsinha0825@gmail.com>
2024-11-21 19:27:51 +0530

    Refactoring the code and adding a middleware

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
 const fs = require('fs');
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
-app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello, world!', app: 'Natours' });
-// });
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint');
-// });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-app.get('/api/v1/tours', (req, res) => {
+//1. Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Middleware: Request received');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//2. Route handlers
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
-    data: { tours },
+    data: {
+      tours,
+    },
   });
-});
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
+};
+const getTour = (req, res) => {
+  // console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => id === el.id);
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object.assign(
+    {
+      id: newId,
+    },
+    req.body
+  );
 
   tours.push(newTour);
   fs.writeFile(
@@ -32,11 +60,97 @@ app.post('/api/v1/tours', (req, res) => {
     (err) => {
       res.status(201).json({
         status: 'success',
-        data: { tour: newTour },
+        data: {
+          tour: newTour,
+        },
       });
     }
   );
-});
+};
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour',
+    },
+  });
+};
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+//3.Routes
+
+//Better formatting than the above
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+//4. Server
 const port = 3000;
 app.listen(port, () => {
   console.log('listening on port');
