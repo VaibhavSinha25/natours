99e5603d71f405c91dc60fc0354d7d32fd2fbbbe
Vaibhav <vaibhavsinha0825@gmail.com>
2024-11-30 19:52:24 +0530

    Connecting and creating database using express connection with mongodb

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-console.log(app.get('env'));
+// console.log(app.get('env'));
 // console.log(process.env);
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {});
+
+//think of it as a class
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+//think of it as a constructor function that creates a new instance of Tour
+const Tour = mongoose.model('Tour', tourSchema);
+
+//think of it as object
+const testTour = new Tour({
+  name: 'Park Camper',
+  price: 999
+});
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => console.log(err));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('listening on port');
