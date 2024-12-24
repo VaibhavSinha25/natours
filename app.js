29aa0e05f6070e2ac6986d57b9862aae8d3ecce1
Vaibhav <vaibhavsinha0825@gmail.com>
2024-12-25 01:57:00 +0530

    New middlewares added

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('Middleware: Request received');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Middleware: Request received');
+//   next();
+// });
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -28,5 +28,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', function(req, res, next) {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server!`
+  });
+});
 //4. Server
 module.exports = app;
